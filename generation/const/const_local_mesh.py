7f4d27d10daa42057f436db4ce0668bc9bf60426
furuhashi <fumifuruhashi@gmail.com>
2023-07-27 17:18:51 +0900

    update

---
+++ generation/const/const_local_mesh.py
@@ -0,0 +1,34 @@
+import math
+from const import const_global_mesh
+
+hGMin = const_global_mesh.hGMin
+
+# hL = math.floor(hGMin/6*100)*0.01       # loal mesh size in the x direction[mm]
+hL = 0.30
+elesizeL = hL
+rxzL = 0.5      # ratio of x - z
+rxyL = 1        # ratio of x - y
+hLz = 15 / 20   # local mesh size in the z direction[mm]
+hLy = rxyL * hL # local mesh size in the y direction[mm]
+
+Localmag = 1
+HL = math.ceil(1.2 * const_global_mesh.hGyMin / hLy * Localmag)         # num.of elements above the crack tip
+aL = math.ceil(2.83 * const_global_mesh.hGMax / elesizeL * Localmag)    # num.of elements behind the crack tip
+lL = 15                                                                 # (Localmag)
+
+Wint = 30.      # width of interpolation
+Hint = 30.      # height of interpolation
+Inint = 3.0     # interval of interpolation
+
+delz = 0.00001  # delta z
+delx = 0.00001  # delta x
+nlc = 1         # num.of iteration when coordinates of the local nodes are determined
+
+elesizezL = hLz
+elesizeyL = hLy
+nback = aL
+nfront = lL
+ndyL = HL
+verin = Wint
+intin = Inint
+horin = Hint
